706fad098599515f0945fa4e46d298dd80235d08
Jaak <jacques.barnard03@gmail.com>
2019-06-12 08:34:02 +0200

    base
    ek het hier weer die basis van die codebook af gecopy dat ek die repo op die web kan kry.

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseComponent } from './base/base.component';
+import { MaterialModule } from './material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaseComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
